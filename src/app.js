741ef618ffab81160f7203f4c97fc9c081ad3ee3
toto <aaa@toto.com>
2018-06-19 01:29:28 +0200

    coto express

--- src/app.js
+++ src/app.js
@@ -1,34 +1,65 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-var HTTP = require('http');
-const URL = require('url');
-// const myURL = url.parse('https://user:pass@sub.host.com:8080/p/a/t/h?query=string#hash');
-var server = HTTP.createServer((request, response) => {
-    const incomingHttpHeaders = request.headers;
-    for (let key in incomingHttpHeaders) {
-        console.log(`${key}: ${incomingHttpHeaders[key]}`);
-    }
-    const { method, url } = request;
-    const { headers } = request;
-    const userAgent = headers['user-agent'];
-    let res = {};
-    res['request'] = {
-        headers: request.headers,
-        httpVersion: request.httpVersion,
-        method: request.method,
-        trailers: request.trailers,
-        statusCode: request.statusCode,
-        statusMessage: request.statusMessage,
-        url: request.url,
-    };
-    res['parsed url'] = URL.parse(request.url);
-    // response.setHeader('Content-Type', 'text/html');
-    response.setHeader('Content-Type', 'application/json');
-    response.setHeader('toto', 'totobar');
-    response.writeHead(200, 'tout est ok !');
-    // response.write(JSON.stringify(request.headers));
-    response.write(JSON.stringify(res));
-    response.end();
+const url_1 = require("url");
+const HTTP = require('http');
+// const URL = require('url');
+const testURL = url_1.parse('https://user:pass@sub.host.com:8080/p/a/t/h?query=string#hash');
+const server = HTTP.createServer((request, response) => {
+    const coto = new Coto(request, response);
+    coto.get('/', () => {
+        const incomingHttpHeaders = request.headers;
+        for (const key in incomingHttpHeaders) {
+            console.log(`${key}: ${incomingHttpHeaders[key]}`);
+        }
+        const { method, url } = request;
+        const { headers } = request;
+        const userAgent = headers['user-agent'];
+        const res = {};
+        res['request'] = {
+            headers: request.headers,
+            httpVersion: request.httpVersion,
+            method: request.method,
+            trailers: request.trailers,
+            statusCode: request.statusCode,
+            statusMessage: request.statusMessage,
+            url: request.url,
+        };
+        res['parsed url'] = url_1.parse(request.url);
+        // res['parsed testURL'] = urlParse(testURL);
+        // response.setHeader('Content-Type', 'text/html');
+        response.setHeader('Content-Type', 'application/json');
+        response.setHeader('toto', 'totobar');
+        response.writeHead(200, 'tout est ok !');
+        // response.write(JSON.stringify(request.headers));
+        response.write(JSON.stringify(res));
+        response.end();
+    });
+    coto.get('/info', (req, res) => {
+        res.setHeader('Content-Type', 'text/html');
+        res.setHeader('Set-Cookie', ['type=ninja', 'language=javascript']);
+        res.writeHead(200);
+        res.write('<h1>Info yeah</h1>');
+        res.end();
+    });
 });
 server.listen(3000);
+class Coto {
+    constructor(request, response) {
+        if (request == null) {
+            throw Error('erreur');
+        }
+        this.request = request;
+        this.response = response;
+    }
+    method(verb, pathname, f) {
+        const { method } = this.request;
+        const urlWithStringQuery = url_1.parse(this.request.url);
+        if (method === verb && urlWithStringQuery.pathname === pathname) {
+            f(this.request, this.response);
+        }
+    }
+    get(pathname, f) {
+        this.method('GET', pathname, f);
+    }
+}
 //# sourceMappingURL=app.js.map
--- src/app.ts
+++ src/app.ts
@@ -1,47 +1,62 @@
-import {IncomingHttpHeaders, IncomingMessage, ServerResponse} from "http";
-
-var HTTP = require('http');
-
-const URL = require('url');
-// const myURL = url.parse('https://user:pass@sub.host.com:8080/p/a/t/h?query=string#hash');
+import { IncomingHttpHeaders, IncomingMessage, ServerResponse } from 'http';
+import { parse as urlParse, UrlWithStringQuery } from 'url';
+const HTTP = require('http');
+// const URL = require('url');
 
 
-var server = HTTP.createServer((request: IncomingMessage, response: ServerResponse) => {
-
-    const incomingHttpHeaders: IncomingHttpHeaders = request.headers;
-
-    for(let key in incomingHttpHeaders){
-        console.log(`${key}: ${incomingHttpHeaders[key]}`);
-    }
-
-    const { method, url } = request;
-    const { headers } = request;
-    const userAgent = headers['user-agent'];
-
-    let res: any = {};
-
-    res['request'] = {
-        headers: request.headers,
-        httpVersion: request.httpVersion,
-        method: request.method,
-        trailers: request.trailers,
-        statusCode: request.statusCode,
-        statusMessage: request.statusMessage,
-        url: request.url,
-    };
+const testURL = urlParse('https://user:pass@sub.host.com:8080/p/a/t/h?query=string#hash');
 
 
-    res['parsed url'] = URL.parse(request.url);
+const server = HTTP.createServer((request: IncomingMessage, response: ServerResponse) => {
 
+    const coto: Coto = new Coto(request, response);
 
-    // response.setHeader('Content-Type', 'text/html');
-    response.setHeader('Content-Type', 'application/json');
-    response.setHeader('toto', 'totobar');
-    response.writeHead(200, 'tout est ok !');
+    coto.get('/', () => {
 
-    // response.write(JSON.stringify(request.headers));
-    response.write(JSON.stringify(res));
-    response.end();
+        const incomingHttpHeaders: IncomingHttpHeaders = request.headers;
+        
+            for (const key in incomingHttpHeaders) {
+                console.log(`${key}: ${incomingHttpHeaders[key]}`);
+            }
+        
+            const { method, url } = request;
+            const { headers } = request;
+            const userAgent = headers['user-agent'];
+        
+            const res: any = {};
+                
+            res['request'] = {
+                headers: request.headers,
+                httpVersion: request.httpVersion,
+                method: request.method,
+                trailers: request.trailers,
+                statusCode: request.statusCode,
+                statusMessage: request.statusMessage,
+                url: request.url,
+            };
+        
+        
+            res['parsed url'] = urlParse(request.url);
+            // res['parsed testURL'] = urlParse(testURL);
+        
+            // response.setHeader('Content-Type', 'text/html');
+            response.setHeader('Content-Type', 'application/json');
+            response.setHeader('toto', 'totobar');
+            response.writeHead(200, 'tout est ok !');
+        
+            // response.write(JSON.stringify(request.headers));
+            response.write(JSON.stringify(res));
+            response.end();
+
+    });
+
+    coto.get('/info', (req: IncomingMessage, res: ServerResponse) => {
+        res.setHeader('Content-Type', 'text/html');
+        res.setHeader('Set-Cookie', ['type=ninja', 'language=javascript']);
+        res.writeHead(200);
+        res.write('<h1>Info yeah</h1>');
+        res.end();
+    });
 
 
 
@@ -49,3 +64,32 @@ var server = HTTP.createServer((request: IncomingMessage, response: ServerRespon
 
 
 server.listen(3000);
+
+
+
+
+class Coto {
+
+    private request: IncomingMessage;
+    private response: ServerResponse;
+
+    constructor(request: IncomingMessage, response: ServerResponse) {
+        if (request == null) { throw Error('erreur'); }
+        this.request = request;
+        this.response = response;
+    }
+
+    private method(verb: string, pathname: string, f: (request: IncomingMessage, response: ServerResponse) => void): void {
+      const { method } = this.request;
+        const urlWithStringQuery: UrlWithStringQuery = urlParse(this.request.url);
+
+        if (method === verb && urlWithStringQuery.pathname === pathname) {
+            f(this.request, this.response);
+        }
+    }
+
+    get(pathname: string, f: (request: IncomingMessage, response: ServerResponse) => void): void {
+        this.method('GET', pathname, f);
+    }
+
+}
